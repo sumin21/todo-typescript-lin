c98af61b172264973feed251635a357a6dea16ec
sumin21 <tnals1178@naver.com>
2021-08-08 21:00:52 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { TodoList } from "./TodoList";
+import { AddTodoForm } from "./AddTodoForm";
+
+const initialTodos: Array<Todo> = [
+  { text: "Walk the dog", complete: true },
+  { text: "Write app", complete: false },
+];
+
+function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo: ToggleTodo = (selectedTodo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const addTodo: AddTodo = (newTodo) => {
+    newTodo.trim() !== "" &&
+      setTodos([...todos, { text: newTodo, complete: false }]);
+  };
+
+  return (
+    <React.Fragment>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
+    </React.Fragment>
+  );
+}
+
+export default App;
